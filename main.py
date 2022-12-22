465d5ce3a4bdb71fb2c4ab01a01335d4de7d8efa
Andrey05319 <65806981+Andrey05319@users.noreply.github.com>
2022-12-22 21:03:04 +0600

    Up

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from jinja2 import Template
 
 name = "Sven"
+age = 47
 
-tm =  Template("Hello {{ name }}")
-msg = tm.render(name=name)
+tm =  Template("I am {{ a }} and my name is {{ n }}")
+msg = tm.render(n=name, a=age)
+# Add name and age parameter into the template
 
 print(msg)
+
+##################################################################
+
+# # Avaliable constructions
+# {%%} - спецификатор шаблона
+# {{}} - выражение для вставки конструкций Python в шаблон
+# {##} - блок комментариев
+# ### - строковый комментарий
