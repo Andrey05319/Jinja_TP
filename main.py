7641d3c312f4871e577cafdaaa1e6fc1918b0072
Andrey05319 <65806981+Andrey05319@users.noreply.github.com>
2022-12-22 21:15:36 +0600

    Up

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from jinja2 import Template
 
-name = "Sven"
-age = 47
+# EXAMPLE 1 ##################################################################
 
-tm =  Template("I am {{ a }} and my name is {{ n }}")
-msg = tm.render(n=name, a=age)
-# Add name and age parameter into the template
+# name = "Sven"
+# age = 47
+#
+# tm = Template("I am {{ a*2 }} and my name is {{ n.upper() }}.")
+# msg = tm.render(n=name, a=age)  # Add name and age parameter into the template
+#
+# print(msg)
 
-print(msg)
-
-##################################################################
-
-# # Avaliable constructions
+# Avaliable constructions ##################################################################
 # {%%} - спецификатор шаблона
 # {{}} - выражение для вставки конструкций Python в шаблон
 # {##} - блок комментариев
 # ### - строковый комментарий
+
+# EXAMPLE 2 ##################################################################
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+per = Person("Sven", 56)
+
+tm = Template("I am {{ p.age }} and my name is {{ p.name }}.")
+msg = tm.render(p=per)
+
+print(msg)
