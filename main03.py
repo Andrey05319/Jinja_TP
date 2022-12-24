fc2dc5b427ac998b1219f124ae3d9c3a8d485df1
Andrey05319 <65806981+Andrey05319@users.noreply.github.com>
2022-12-24 13:21:21 +0600

    Up

--- main03.py
+++ main03.py
@@ -5,16 +5,36 @@ from jinja2 import Template
 # EXAMPLE 1 ##################################################################
 # SUM ##################################################################
 
-cars = [
-    {'model': "Huyndai", 'price': 10001},
-    {'model': "Aydi", 'price': 20002},
-    {'model': "Opel", 'price': 30003},
-    {'model': "Kia", 'price': 40004},
-]
+# # Шаблон:
+# # sum - вычисление суммы поля в коллекции
+# # sum(iterable, attribute=None, start=0)
+#
+# cars = [
+#     {'model': "Huyndai", 'price': 10001},
+#     {'model': "Aydi", 'price': 20002},
+#     {'model': "Opel", 'price': 30003},
+#     {'model': "Kia", 'price': 40004},
+# ]
+#
+# tpl = "Суммарная цена автомобилей {{ cs | sum(attribute='price')}}"
+#
+# tm = Template(tpl)
+# msg = tm.render(cs=cars)
+#
+# print(msg)
 
-tpl = "Суммарная цена автомобилей {{ cs | sum(attribute='price')}}"
+# EXAMPLE 2 ##################################################################
+# SUM ##################################################################
+
+# Шаблон:
+# sum - вычисление суммы поля в коллекции
+# sum(iterable, attribute=None, start=0)
+
+digs = [1,2,3,4,5]
+
+tpl = "Суммарная цена автомобилей {{ cs | sum }}"
 
 tm = Template(tpl)
-msg = tm.render(cs=cars)
+msg = tm.render(cs=digs)
 
 print(msg)
