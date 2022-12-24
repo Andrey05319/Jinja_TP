ce14ff4c679ebc8be07936360a18ddb5c7ff689e
Andrey05319 <65806981+Andrey05319@users.noreply.github.com>
2022-12-24 13:28:54 +0600

    Up

--- main03.py
+++ main03.py
@@ -26,15 +26,45 @@ from jinja2 import Template
 # EXAMPLE 2 ##################################################################
 # SUM ##################################################################
 
+# # Шаблон:
+# # sum - вычисление суммы поля в коллекции
+# # sum(iterable, attribute=None, start=0)
+#
+# digs = [1,2,3,4,5]
+#
+# tpl = "Суммарная цена автомобилей {{ cs | sum }}"
+#
+# tm = Template(tpl)
+# msg = tm.render(cs=digs)
+#
+# print(msg)
+
+# EXAMPLE 3 ##################################################################
+# MAX, MIN ##################################################################
+
 # Шаблон:
 # sum - вычисление суммы поля в коллекции
 # sum(iterable, attribute=None, start=0)
 
-digs = [1,2,3,4,5]
+cars = [
+    {'model': "Huyndai", 'price': 10001},
+    {'model': "Aydi", 'price': 20002},
+    {'model': "Opel", 'price': 30003},
+    {'model': "Kia", 'price': 40004},
+]
 
-tpl = "Суммарная цена автомобилей {{ cs | sum }}"
+# tpl = "Максимальная цена автомобиля {{ cs | max(attribute='price')}}" # выдаст элемент словаря
+# tpl = "Максимальная цена автомобиля {{ (cs | max(attribute='price')).price }}" # выдаст только стоимость
+tpl = "Минимальная цена автомобиля {{ (cs | min(attribute='price')).price }}" # выдаст только стоимость
+
 
 tm = Template(tpl)
-msg = tm.render(cs=digs)
+msg = tm.render(cs=cars)
 
 print(msg)
+
+
+
+
+
+
